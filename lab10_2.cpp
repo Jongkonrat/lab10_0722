7f5d0a4f2bda8ed821cece98b580d0d80aff35d5
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-01-12 18:00:29 +0700

    completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -2,7 +2,6 @@
 #include<string>
 #include<cstdlib>
 #include<ctime>
-
 using namespace std;
 
 string cardNames[] = {"","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
@@ -10,34 +9,8 @@ int cardScores[] = {0,1,2,3,4,5,6,7,8,9,10,10,10,10};
 
 int drawCard(void){
 	//Write the function to random the number from 1 to 13 and return that random number.
-	int i = rand()%13;
-	if(i==0){
-		return 1;
-	}else if(i==1){
-		return 2;
-	}else if(i==2){
-		return 3;
-	}else if(i==3){
-		return 4;
-	}else if(i==4){
-		return 5;
-	}else if(i==5){
-		return 6;
-	}else if(i==6){
-		return 7;
-	}else if(i==7){
-		return 8;
-	}else if(i==8){
-		return 9;
-	}else if(i==9){
-		return 10;
-	}else if(i==10){
-		return 11;
-	}else if(i==11){
-		return 12;
-	}else{
-		return 13;
-	}
+	int i = rand()%13+1;
+	return i;
 }
 
 int calScore(int x,int y,int z){
@@ -72,30 +45,33 @@ void checkWinner(int p, int y){
 		cout <<   "|             Draw!!!           |"; // when p is equal to y
 	}else if(p>y){
 		cout <<   "|         Player wins!!!        |"; // when p is greater than y
-	}else if(p<y){
+	}else{
 		cout <<   "|          Yugi wins!!!         |"; // when p is less than y
 	}
 	cout << "\n---------------------------------\n";
 }
 
 int main(){	
+	srand(time(0));
 	int playerScore, yugiScore, playerAction, yugiAction;
-	int playerCards[3] = {drawCard(), drawCard(), drawCard()};
-	int yugiCards[3] = {drawCard(),drawCard(),drawCard()}; //This line of code is not completed. You need to initialize value of yugiCards[].
+	int playerCards[3] = {drawCard(), drawCard(), 0};
+	int yugiCards[3] = {drawCard(),drawCard(),0}; //This line of code is not completed. You need to initialize value of yugiCards[].
 	
 	cout << "---------ORE NO TURN, DRAW!!!------------\n";
 	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
-	playerScore = calScore(playerCards[0],playerCards[1],0);
+	playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
 	cout << "Your score: " << playerScore << "\n";
 	do{
 		cout << "(1) Destiny draw (2) Stay, SELECT: ";
 		cin >> playerAction;
-	}while(playerAction==0);//This line of code is not completed. You need to set the condition to do loop if user's input is not 1 or 2. 
+	}while(playerAction!=1&&playerAction!=2);//This line of code is not completed. You need to set the condition to do loop if user's input is not 1 or 2. 
 	if(playerAction == 1){
 		//The following lines of code are not completed. Please correct it.
 		cout << "Player draws the 3rd card!!!" << "\n";
+		playerCards[2] = drawCard();
 		cout << "Your 3rd card: " << cardNames[playerCards[2]] << "\n";
-		cout << "Your new score: " << calScore(playerCards[0],playerCards[1],playerCards[2]) << "\n";
+		playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+		cout << "Your new score: " << playerScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n\n";
 	
@@ -104,16 +80,19 @@ int main(){
 	
 	cout << "---------YUGI'S TURN, DRAW!!!------------\n";
 	cout << "Yugi's cards: " << cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]] << "\n";
-	yugiScore = calScore(yugiCards[0],yugiCards[1],0);
+	yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
 	cout << "Yugi's score: " << yugiScore << "\n";
 	yugiAction = findYugiAction(yugiScore);
 	if(yugiAction == 1){
 		cout << "Yugi draws the 3rd card!!!\n";
+		yugiCards[2] = drawCard();
 		cout << "Yugi's 3rd card: " << cardNames[yugiCards[2]] << "\n";
-		cout << "Yugi's new score: " << calScore(yugiCards[0],yugiCards[1],yugiCards[2]) <<"\n";
+		yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
+		cout << "Yugi's new score: " << yugiScore <<"\n";
 	}
 	cout << "------------ Turn end -------------------\n";
 	
 	
 	checkWinner(playerScore,yugiScore);
+	return 0;
 }
